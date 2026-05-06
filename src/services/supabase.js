7b9c25c85cfeb6633e54200478d25e0982e2869b
Dhruv G Nayak <dhruvg.cs24@gmail.com>
2026-05-06 22:17:42 +0530

    Removed hardcoded Supabase keys

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,8 +3,8 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://otforjmlbojmndfnsrsd.supabase.co'
-const supabaseAnonKey = 'sb_publishable_u9nEXKhkkjC5iQXgAGaLSw_vh5eAttm'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
